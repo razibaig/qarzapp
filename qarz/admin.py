dcd87417b71f4a8d89c9170abc1b09fbfd47ea99
razibaig <mrrazi.baig@gmail.com>
2021-06-29 17:28:28 +0500

    Added django admin related changes.
    Signed-off-by: razibaig <mrrazi.baig@gmail.com>

--- qarz/admin.py
+++ qarz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import QarzUser, Transaction, Report
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('cnic', 'name', 'mobile', 'type')
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('qarz_user', 'transaction_date', 'amount', 'donation_or_loan')
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Report._meta.get_fields()]
+
+
+admin.site.register(QarzUser, UserAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Report, ReportAdmin)
